49cf304db892d934b9c7439a0cd9751eeccc217a
dankyui <danky@dankys-MacBook-Air.local>
2017-11-22 15:47:07 +0800

    201711221547

--- core/config/config.dev.js
+++ core/config/config.dev.js
@@ -9,6 +9,6 @@ config.dbPort = process.env.dbPort || '53015'
 config.dbName = process.env.dbName || 's381f-mongo'
 config.dbUser = process.env.dbUser || 'dbuser'
 config.dbPassword = process.env.dbPassword || 'P%40ssw0rd'
-config.serverPort = process.env.serverPort || 3000
+config.serverPort = process.env.PORT || 3000
 
 export default config
